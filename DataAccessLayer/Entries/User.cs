aca70de73c30e112088b9508d2b31f5fc4af9ca8
ReshiWiz <nanthamjevz123@gmail.com>
2024-02-09 03:52:42 +0530

    Add project files.

---
+++ DataAccessLayer/Entries/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Entries
+{
+    public class User
+    {
+        public int User_Id { get; set; }
+        public string? User_Name { get; set; }
+        public string? User_Email { get; set; }
+        public string? User_Password { get; set; }
+        public string? User_Phone { get; set; }
+        public string? User_Dept { get; set; }
+  
+    }
+}
---
+++ UI_SetUpOfLearnNext/Controllers/Admin/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UI_SetUpOfLearnNext.Controllers.AnimePage
+{
+	public class AdminController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
